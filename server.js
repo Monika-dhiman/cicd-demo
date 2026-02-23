a03421b27f3c36089f1f0131e171068c8234f72f
Monika <monika.dhiman@bridgingtech.com>
2026-02-23 12:43:51 +0530

    Trigger deployment test again

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // Simple Routes
 app.get("/", (req, res) => {
   res.json({
-    message: "CI/CD with PM2 working 🚀",
+    message: "CI/CD with PM2 working.... 🚀",
     pid: process.pid,
     cpu: os.cpus().length
   });
