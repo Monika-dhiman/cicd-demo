8ab654eef9bdf7ee39233aeb148e81bc8e0ed989
Monika <monika.dhiman@bridgingtech.com>
2026-02-23 10:36:39 +0530

    Initialize project with essential files: .gitignore, README.md, ecosystem.config.js, package.json, and server.js

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "pm2-cicd-demo-app",
+      script: "server.js",
+      //   instances: "max", // Use all available CPU cores for clustering
+      instances: 1,
+      //   exec_mode: "cluster", // Use cluster mode for load balancing
+      exec_mode: "fork",
+      watch: false,
+      max_memory_restart: "300M",
+      env: {
+        NODE_ENV: "dev",
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const os = require("os");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Simple Routes
+app.get("/", (req, res) => {
+  res.json({
+    message: "PM2 Demo App Running 🚀",
+    pid: process.pid,
+    cpu: os.cpus().length
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    uptime: process.uptime()
+  });
+});
+
+app.get("/crash", (req, res) => {
+  res.json({ message: "App will crash now 💥" });
+  process.exit(1); // simulate crash
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT || 3000} with PID ${process.pid}.`);
+});
